c7105c4db3b3f6196a07b7b872d23c14835e1e7e
Arcival Palma <palma_arcival@yahoo.com>
2021-08-17 04:19:37 +0000

    create input

---
+++ input.js
@@ -0,0 +1,22 @@
+const handleUserInput = function(data) {
+  if (data === '\u0003') {
+    process.exit();
+  }
+}
+
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  process.stdin.on("data", handleUserInput);
+
+  return stdin;
+}
+
+module.exports = { setupInput };
--- play.js
+++ play.js
@@ -1,29 +1,6 @@
 // play.js
 const { connect } = require('./client');
-
-const handleUserInput = function(data) {
-
-  if (data === '\u0003') {
-    process.exit();
-  }
-
-  console.log(data)
-}
-
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-
-  process.stdin.on("data", handleUserInput);
-
-  return stdin;
-}
+const { setupInput } = require('./input');
 
 setupInput();
 
