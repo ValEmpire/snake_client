81a3e5ecf2746cfcc084569f3909dc3b16955fba
Arcival Palma <palma_arcival@yahoo.com>
2021-08-17 03:05:30 +0000

    client

--- play.js
+++ play.js
@@ -1,23 +1,5 @@
-const net = require('net');
-
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-
-  conn.on('data', (m)=>{
-    console.log(m);
-  })
-
-  return conn;
-}
+// play.js
+const { connect } = require('./client');
 
 console.log('Connecting ...');
 
